626d3f4aba2f98888af56837adeb8ce976f33f67
Arleta Jędrzejczak <laililang@gmail.com>
2017-06-06 01:03:43 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,42 @@ $('#first').click(function(){
           $('#recipe').css("display", "block");
     }
 });
+
+$('#second').mouseover(function(){
+    $('#second img').css("opacity", 0.2);
+    $('#second p').css("visibility", "visible")
+});
+
+$('#second').mouseout(function(){
+    $('#second img').css("opacity", 1);
+    $('#second p').css("visibility", "hidden")
+});
+
+$('#second').click(function(){
+    if($('#recipe2').css("display")=="block"){
+        $('#recipe2').css("display", "none");
+    }
+    else{
+          $('#recipe2').css("display", "block");
+    }
+});
+
+
+$('#third').mouseover(function(){
+    $('#third img').css("opacity", 0.2);
+    $('#third p').css("visibility", "visible")
+});
+
+$('#third').mouseout(function(){
+    $('#third img').css("opacity", 1);
+    $('#third p').css("visibility", "hidden")
+});
+
+$('#third').click(function(){
+    if($('#recipe3').css("display")=="block"){
+        $('#recipe3').css("display", "none");
+    }
+    else{
+          $('#recipe3').css("display", "block");
+    }
+});
