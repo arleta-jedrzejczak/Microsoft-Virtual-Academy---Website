35006cb68c29a9977372e1892495179345f26c96
Arleta Jędrzejczak <laililang@gmail.com>
2017-06-05 23:33:14 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,12 @@ $('#first').mouseout(function(){
     $('#first img').css("opacity", 1);
     $('#first p').css("visibility", "hidden")
 });
+
+$('#first').click(){
+    if($('#recipe').css("display")=="block"){
+        $('#recipe').css("display", "none");
+    }
+    else{
+          $('#recipe').css("display", "block");
+    }
+})
